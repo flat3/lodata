1ef711c229dd4bba3dc7de93d8a7016d77ad86dc
Chris Lloyd <chrislloyd403@gmail.com>
2020-10-18 13:45:14 +0100

    WIP

--- src/DeclaredProperty.php
+++ src/DeclaredProperty.php
@@ -4,4 +4,69 @@ namespace Flat3\Lodata;
 
 class DeclaredProperty extends Property
 {
+    /** @var bool $searchable Whether this property is included as part of $search requests */
+    protected $searchable = false;
+
+    /** @var bool $filterable Whether this property can be used in a $filter expression */
+    protected $filterable = true;
+
+    /** @var bool $alternativeKey Whether this property can be used as an alternative key */
+    protected $alternativeKey = false;
+
+    public function isAlternativeKey(): bool
+    {
+        return $this->alternativeKey;
+    }
+
+    public function setAlternativeKey(bool $alternativeKey = true): self
+    {
+        $this->alternativeKey = $alternativeKey;
+
+        return $this;
+    }
+
+    /**
+     * Get whether this property is included in search
+     *
+     * @return bool
+     */
+    public function isSearchable(): bool
+    {
+        return $this->searchable;
+    }
+
+    /**
+     * Set whether this property is included in search
+     *
+     * @param  bool  $searchable
+     *
+     * @return $this
+     */
+    public function setSearchable(bool $searchable = true): self
+    {
+        $this->searchable = $searchable;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isFilterable(): bool
+    {
+        return $this->filterable;
+    }
+
+    /**
+     * @param  bool  $filterable
+     *
+     * @return Property
+     */
+    public function setFilterable(bool $filterable): Property
+    {
+        $this->filterable = $filterable;
+
+        return $this;
+    }
+
 }
--- src/Property.php
+++ src/Property.php
@@ -17,15 +17,6 @@ abstract class Property implements TypeInterface, NameInterface
     /** @var bool $nullable Whether this property is nullable */
     protected $nullable = true;
 
-    /** @var bool $searchable Whether this property is included as part of $search requests */
-    protected $searchable = false;
-
-    /** @var bool $filterable Whether this property can be used in a $filter expression */
-    protected $filterable = true;
-
-    /** @var bool $alternativeKey Whether this property can be used as an alternative key */
-    protected $alternativeKey = false;
-
     public function __construct($name, TypeInterface $type)
     {
         $this->setName($name);
@@ -40,50 +31,6 @@ abstract class Property implements TypeInterface, NameInterface
                 sprintf('A dynamic property must implement %s', DynamicPropertyInterface::class)
             );
         }
-    }
-
-    /**
-     * Get whether this property is included in search
-     *
-     * @return bool
-     */
-    public function isSearchable(): bool
-    {
-        return $this->searchable;
-    }
-
-    /**
-     * Set whether this property is included in search
-     *
-     * @param  bool  $searchable
-     *
-     * @return $this
-     */
-    public function setSearchable(bool $searchable = true): self
-    {
-        $this->searchable = $searchable;
-
-        return $this;
-    }
-
-    /**
-     * @return bool
-     */
-    public function isFilterable(): bool
-    {
-        return $this->filterable;
-    }
-
-    /**
-     * @param  bool  $filterable
-     *
-     * @return Property
-     */
-    public function setFilterable(bool $filterable): Property
-    {
-        $this->filterable = $filterable;
-
-        return $this;
     }
 
     public function isNullable(): bool
@@ -101,18 +48,6 @@ abstract class Property implements TypeInterface, NameInterface
     public function setNullable(bool $nullable): self
     {
         $this->nullable = $nullable;
-
-        return $this;
-    }
-
-    public function isAlternativeKey(): bool
-    {
-        return $this->alternativeKey;
-    }
-
-    public function setAlternativeKey(bool $alternativeKey = true): self
-    {
-        $this->alternativeKey = $alternativeKey;
 
         return $this;
     }
