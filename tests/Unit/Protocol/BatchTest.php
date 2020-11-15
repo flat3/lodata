c821be7bd06040d0333e6ef47b16d45484a1adf4
Chris Lloyd <chrislloyd403@gmail.com>
2020-11-15 19:14:09 +0000

    WIP

--- tests/Unit/Protocol/BatchTest.php
+++ tests/Unit/Protocol/BatchTest.php
@@ -2,8 +2,13 @@
 
 namespace Flat3\Lodata\Tests\Unit\Protocol;
 
+use Flat3\Lodata\Facades\Lodata;
+use Flat3\Lodata\Interfaces\Operation\ActionInterface;
+use Flat3\Lodata\Interfaces\Operation\FunctionInterface;
+use Flat3\Lodata\Operation;
 use Flat3\Lodata\Tests\Request;
 use Flat3\Lodata\Tests\TestCase;
+use Flat3\Lodata\Type\Int32;
 
 class BatchTest extends TestCase
 {
@@ -67,6 +72,108 @@ MULTIPART
         );
     }
 
+    public function test_service_document()
+    {
+        $this->assertTextMetadataResponse(
+            Request::factory()
+                ->path('/$batch')
+                ->header('content-type', 'multipart/mixed; boundary=batch_36522ad7-fc75-4b56-8c71-56071383e77b')
+                ->post()
+                ->multipart(<<<MULTIPART
+--batch_36522ad7-fc75-4b56-8c71-56071383e77b
+Content-Type: application/http
+
+GET /odata/
+Host: localhost
+
+
+--batch_36522ad7-fc75-4b56-8c71-56071383e77b
+MULTIPART
+                )
+        );
+    }
+
+    public function test_metadata_document()
+    {
+        $this->assertTextMetadataResponse(
+            Request::factory()
+                ->path('/$batch')
+                ->header('content-type', 'multipart/mixed; boundary=batch_36522ad7-fc75-4b56-8c71-56071383e77b')
+                ->post()
+                ->multipart(<<<'MULTIPART'
+--batch_36522ad7-fc75-4b56-8c71-56071383e77b
+Content-Type: application/http
+
+GET /odata/$metadata
+Host: localhost
+
+
+--batch_36522ad7-fc75-4b56-8c71-56071383e77b
+MULTIPART
+                )
+        );
+    }
+
+    public function test_action_invocation()
+    {
+        Lodata::add(new class('aa1') extends Operation implements ActionInterface {
+            public function invoke(Int32 $a, Int32 $b): Int32
+            {
+                return new Int32($a->get() + $b->get());
+            }
+        });
+
+        $this->assertTextMetadataResponse(
+            Request::factory()
+                ->path('/$batch')
+                ->header('content-type', 'multipart/mixed; boundary=batch_36522ad7-fc75-4b56-8c71-56071383e77b')
+                ->post()
+                ->multipart(<<<'MULTIPART'
+--batch_36522ad7-fc75-4b56-8c71-56071383e77b
+Content-Type: application/http
+
+POST /odata/aa1
+Host: localhost
+Content-Type: application/json
+
+{
+  "a": 3,
+  "b": 4
+}
+
+--batch_36522ad7-fc75-4b56-8c71-56071383e77b
+MULTIPART
+                )
+        );
+    }
+
+    public function test_function_invocation()
+    {
+        Lodata::add(new class('aa1') extends Operation implements FunctionInterface {
+            public function invoke(Int32 $a, Int32 $b): Int32
+            {
+                return new Int32($a->get() + $b->get());
+            }
+        });
+
+        $this->assertTextMetadataResponse(
+            Request::factory()
+                ->path('/$batch')
+                ->header('content-type', 'multipart/mixed; boundary=batch_36522ad7-fc75-4b56-8c71-56071383e77b')
+                ->post()
+                ->multipart(<<<'MULTIPART'
+--batch_36522ad7-fc75-4b56-8c71-56071383e77b
+Content-Type: application/http
+
+GET /odata/aa1(a=3,b=4)
+Host: localhost
+
+--batch_36522ad7-fc75-4b56-8c71-56071383e77b
+MULTIPART
+                )
+        );
+    }
+
     public function test_relative_path()
     {
         $this->assertTextMetadataResponse(
@@ -100,6 +207,27 @@ MULTIPART
 Content-Type: application/http
 
 GET notfound
+Host: localhost
+
+
+--batch_36522ad7-fc75-4b56-8c71-56071383e77b
+MULTIPART
+                )
+        );
+    }
+
+    public function test_bad_request()
+    {
+        $this->assertTextMetadataResponse(
+            Request::factory()
+                ->path('/$batch')
+                ->header('content-type', 'multipart/mixed; boundary=batch_36522ad7-fc75-4b56-8c71-56071383e77b')
+                ->post()
+                ->multipart(<<<MULTIPART
+--batch_36522ad7-fc75-4b56-8c71-56071383e77b
+Content-Type: application/http
+
+GET flights('a')
 Host: localhost
 
 
