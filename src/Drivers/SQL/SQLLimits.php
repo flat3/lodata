ac924f8882df4f255e79beb9f53295cbf02afdc4
Merouane KHALILI <1122628+merouanekhalili@users.noreply.github.com>
2021-11-15 10:43:12 +0100

    Fixes how limits are handled in SQL for PostgreSQL

--- src/Drivers/SQL/SQLLimits.php
+++ src/Drivers/SQL/SQLLimits.php
@@ -22,7 +22,13 @@ trait SQLLimits
             return '';
         }
 
+
         $limits = ' LIMIT ?,?';
+
+        switch ($this->getDriver()) {
+            case 'pgsql':
+                $limits = ' LIMIT ? OFFSET ?';
+        }
 
         $this->addParameter($this->getSkip()->getValue());
         $this->addParameter($this->getTop()->hasValue() ? $this->getTop()->getValue() : PHP_INT_MAX);
