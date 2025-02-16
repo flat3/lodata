f1fd518fe79dffddfa8776892398566e6b93aa2b
Chris Lloyd <chrislloyd403@gmail.com>
2025-02-16 09:45:01 +0000

    WIP rebased

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -14,20 +14,36 @@ use Flat3\Lodata\Helper\Filesystem;
 use Flat3\Lodata\Helper\Flysystem;
 use Flat3\Lodata\Helper\DBAL;
 use Flat3\Lodata\Helper\Symfony;
+use Illuminate\Database\ConnectionInterface;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpKernel\Kernel;
 
 /**
  * Service Provider
- *
- * https://<server>:<port>/<prefix>/<service-uri>/$metadata
- *
  * @link https://laravel.com/docs/8.x/providers
  * @package Flat3\Lodata
  */
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
 {
+    /**
+     * Get the endpoint of the OData service document
+     * @return string
+     */
+    public static function endpoint(): string
+    {
+        return url(self::route()).'/';
+    }
+
+    /**
+     * Get the configured route prefix
+     * @return string
+     */
+    public static function route(): string
+    {
+        return rtrim(config('lodata.prefix'), '/');
+    }
+
     /**
      * Service provider registration method
      */
@@ -44,32 +60,16 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
         if ($this->app->runningInConsole()) {
             $this->publishes([__DIR__.'/../config.php' => config_path('lodata.php')], 'config');
         }
-        $this->bootServices(new Endpoint(''));
-    }
-
-    private function bootServices(Endpoint $service): void
-    {
-        // register the $service, which is a singleton, with the container; this allows us
-        // to fulfill all old ServiceProvider::route() and ServiceProvider::endpoint()
-        // calls with app()->make(ODataService::class)->route() or
-        // app()->make(ODataService::class)->endpoint()
-        $this->app->instance(Endpoint::class, $service);
-
-        $this->app->bind(DBAL::class, function (Application $app, array $args) {
-            return version_compare(InstalledVersions::getVersion('doctrine/dbal'), '4.0.0', '>=') ? new DBAL\DBAL4($args['connection']) : new DBAL\DBAL3($args['connection']);
-        });
 
         $this->loadJsonTranslationsFrom(__DIR__.'/../lang');
 
-        // next instantiate and discover the global Model
-        $model = $service->discover(new Model());
-        assert($model instanceof Model);
+        $this->app->singleton(Model::class, function () {
+            return new Model();
+        });
 
-        // and register it with the container
-        $this->app->instance(Model::class, $model);
-
-        // register alias
-        $this->app->alias(Model::class, 'lodata.model');
+        $this->app->bind('lodata.model', function ($app) {
+            return $app->make(Model::class);
+        });
 
         $this->app->bind(Response::class, function () {
             return Kernel::VERSION_ID < 60000 ? new Symfony\Response5() : new Symfony\Response6();
@@ -79,7 +79,11 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
             return class_exists('League\Flysystem\Adapter\Local') ? new Flysystem\Flysystem1() : new Flysystem\Flysystem3();
         });
 
-        $route = $service->route();
+        $this->app->bind(DBAL::class, function (Application $app, array $args) {
+            return version_compare(InstalledVersions::getVersion('doctrine/dbal'), '4.0.0', '>=') ? new DBAL\DBAL4($args['connection']) : new DBAL\DBAL3($args['connection']);
+        });
+
+        $route = self::route();
         $middleware = config('lodata.middleware', []);
 
         Route::get("{$route}/_lodata/odata.pbids", [PBIDS::class, 'get']);
