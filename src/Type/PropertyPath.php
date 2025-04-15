c4812e5989435eee144d611b2e5fdab2fc415fbb
Michael Gerzabek <michael.gerzabek@gmail.com>
2025-04-15 15:52:58 +0200

    Integrate alias into $metadata
    - added missing type PropertyPath
    - added Identifier to Record
    - added missing Type attribute to Record
    - added alias to Identifier – including dirty hack to derive it from given namespace
    - added addReference to Lodata Facade to allow adding additional vocabularies to the model
    - added alias for all auto registered References

---
+++ src/Type/PropertyPath.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Flat3\Lodata\Type;
+
+use Flat3\Lodata\Type\String_;
+
+class PropertyPath extends String_
+{
+    // @PropertyPath in EDXML
+    const identifier = 'Edm.PropertyPath';
+}
